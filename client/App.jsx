2413b8eb1ab95b6700c6aeaf0dfc971a82e698da
bryan <bbbryan14@yahoo.com>
2018-12-15 09:50:11 -0500

    working mvp

--- client/App.jsx
+++ client/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Container from './Container.jsx';
-import { runInThisContext } from 'vm';
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            images: [],
+            images: '',
             uploading: false,
             url: ''
         }
@@ -18,19 +17,25 @@ class App extends React.Component {
     }
 
     addToState(image) {
-        const img = this.state.images.slice();
+        // const img = this.state.images.slice();
         const holder = URL.createObjectURL(image);
-        img.push(holder);
-        this.setState({images: img});
+        // img.push(holder);
+        this.setState({images: holder});
     }
 
     submit(){
         let reqBody = new FormData();
 
-        for (let i = this.state.images.length-1; i > -1; i--) {
-            let a = new Blob([this.state.images[i]], {type: 'image/png'});
-            reqBody.append(`${i}`, a);
-        }
+        // for (let i = this.state.images.length-1; i > -1; i--) {
+        //     let a = new Blob([this.state.images[i]], {type: 'image/png'});
+        //     reqBody.append(`${i}`, a);
+        // }
+        const blob = new Blob([this.state.images], {type: 'image/png'})
+        reqBody.append('image', blob, 'image.png');
+
+        // for (let i of reqBody.entries()){
+        //     console.log(i)
+        // }
 
         reqBody.append('url', this.state.url)
 
@@ -50,7 +55,7 @@ class App extends React.Component {
             console.log(result);
             this.setState({
                 uploading: false,
-                images:[]
+                images:''
             })
         })
         .catch(err => console.log(err))
@@ -61,9 +66,9 @@ class App extends React.Component {
     }
 
     removeImg(){
-        const arr = this.state.images.slice();
-        arr.pop();
-        this.setState({images: arr})
+        // const arr = this.state.images.slice();
+        // arr.pop();
+        this.setState({images: ''})
     }
 
     render() {
--- client/Upload.jsx
+++ client/Upload.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function Upload (props) {
     return (
         <div>
-            <form>
-                <input type='file' onChange={e=>{
+            <form action='http://localhost:3000/' method='POST' encType='multipart/form-data'>
+                {/* <input type='file' onChange={e=>{
                     e.persist();
-                    console.log(e.target.files);
+                    // console.log(e.target.files);
                     props.addToState(e.target.files['0']);
                 }}/>
                 <input type='submit' onClick={(e)=> {
@@ -20,8 +20,9 @@ function Upload (props) {
                 <input type='submit' onClick={e=>{
                     e.preventDefault();
                     props.submit();
-                }}/>
-
+                }}/> */}
+                <input name='image' type='file'></input>
+                <input type='submit'></input>    
             </form>
         </div>
     )
